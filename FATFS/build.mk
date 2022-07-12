b168a7ae001a73b859d3ccdad06978f7678d0b5d
XiaoHanliu <liuxiaohan@nucleisys.com>
2022-07-12 13:59:44 +0800

    fatfs file transplant

---
+++ FATFS/build.mk
@@ -0,0 +1,8 @@
+C_SRCDIRS += $(NUCLEI_SDK_MIDDLEWARE)/FATFS
+# heap management selection, choose 1 from the portable/MemMang/heap_*.c
+# C_SRCS += $(NUCLEI_SDK_RTOS)/Source/portable/MemMang/heap_4.c
+# C_SRCS += $(NUCLEI_SDK_RTOS)/Source/portable/GCC/port.c
+
+# ASM_SRCDIRS += $(NUCLEI_SDK_RTOS)/Source/portable/GCC
+
+INCDIRS += $(NUCLEI_SDK_MIDDLEWARE)/FATFS
