63374239ad4ecd93cc34f7d8e5599ab7b7e69d57
XiaoHanliu <liuxiaohan@nucleisys.com>
2022-07-12 14:25:55 +0800

    format modify

--- FATFS/diskio.c
+++ FATFS/diskio.c
@@ -8,123 +8,133 @@
 /*-----------------------------------------------------------------------*/
 
 #include "nuclei_sdk_hal.h"
-#include "diskio.h"		/* FatFs lower layer API */
+#include "diskio.h" /* FatFs lower layer API */
 #include "malloc.h"
 
-#define SD_CARD	 0  
-extern  SD_CardInfo SDCardInfo;
+#define SD_CARD 0
+extern SD_CardInfo SDCardInfo;
 
-DSTATUS disk_initialize (
-	BYTE pdrv				/* Physical drive nmuber (0..) */
+DSTATUS disk_initialize(
+    BYTE pdrv /* Physical drive nmuber (0..) */
 )
 {
-	uint8_t res=0;
-	
-			res=             SD_Init();
-  		
-	if(res)return  STA_NOINIT;
-	else return 0; 
+    uint8_t res = 0;
+
+    res = SD_Init();
+
+    if (res)
+        return STA_NOINIT;
+    else
+        return 0;
 }
 
-DSTATUS disk_status (
-	BYTE pdrv		/* Physical drive nmuber (0..) */
+DSTATUS disk_status(
+    BYTE pdrv /* Physical drive nmuber (0..) */
 )
 {
-	return 0;
+    return 0;
 }
 
-DRESULT disk_read (
-	BYTE pdrv,		/* Physical drive nmuber (0..) */
-	BYTE *buff,		/* Data buffer to store read data */
-	DWORD sector,	/* Sector address (LBA) */
-	UINT count		/* Number of sectors to read (1..BLKSIZE) */
+DRESULT disk_read(
+    BYTE pdrv,    /* Physical drive nmuber (0..) */
+    BYTE *buff,   /* Data buffer to store read data */
+    DWORD sector, /* Sector address (LBA) */
+    UINT count    /* Number of sectors to read (1..BLKSIZE) */
 )
 {
-	uint8_t res=0;
-    if (!count)return RES_PARERR;
-	switch(pdrv)
-	{
-		case SD_CARD:
-			res=SD_ReadDisk(buff,sector,count);
-			while(res)
-			{
-                SD_Init();
-				res=SD_ReadDisk(buff,sector,count);
-			}
-			break;
-		default:
-			res=1;
-	}
-    if(res==0x00)return RES_OK;
-    else return RES_ERROR;
+    uint8_t res = 0;
+    if (!count)
+        return RES_PARERR;
+    switch (pdrv)
+    {
+    case SD_CARD:
+        res = SD_ReadDisk(buff, sector, count);
+        while (res)
+        {
+            SD_Init();
+            res = SD_ReadDisk(buff, sector, count);
+        }
+        break;
+    default:
+        res = 1;
+    }
+    if (res == 0x00)
+        return RES_OK;
+    else
+        return RES_ERROR;
 }
 
 #if _USE_WRITE
-DRESULT disk_write (
-	BYTE pdrv,			/* Physical drive nmuber (0..) */
-	const BYTE *buff,	/* Data to be written */
-	DWORD sector,		/* Sector address (LBA) */
-	UINT count			/* Number of sectors to write (1..BLKSIZE) */
+DRESULT disk_write(
+    BYTE pdrv,        /* Physical drive nmuber (0..) */
+    const BYTE *buff, /* Data to be written */
+    DWORD sector,     /* Sector address (LBA) */
+    UINT count        /* Number of sectors to write (1..BLKSIZE) */
 )
 {
-	uint8_t res=0;
-    if (!count)return RES_PARERR;
-	switch(pdrv)
-	{
-		case SD_CARD:
-			res=SD_WriteDisk((uint8_t*)buff,sector,count);
-			while(res)
-			{
-				SD_Init();	
-				res=SD_WriteDisk((uint8_t*)buff,sector,count);
-			}
-			break;
-		default:
-			res=1;
-	}
-    
-    if(res == 0x00)return RES_OK;
-    else return RES_ERROR;
+    uint8_t res = 0;
+    if (!count)
+        return RES_PARERR;
+    switch (pdrv)
+    {
+    case SD_CARD:
+        res = SD_WriteDisk((uint8_t *)buff, sector, count);
+        while (res)
+        {
+            SD_Init();
+            res = SD_WriteDisk((uint8_t *)buff, sector, count);
+        }
+        break;
+    default:
+        res = 1;
+    }
+
+    if (res == 0x00)
+        return RES_OK;
+    else
+        return RES_ERROR;
 }
 #endif
 
 #if _USE_IOCTL
-DRESULT disk_ioctl (
-	BYTE pdrv,		/* Physical drive nmuber (0..) */
-	BYTE cmd,		/* Control code */
-	void *buff		/* Buffer to send/receive control data */
+DRESULT disk_ioctl(
+    BYTE pdrv, /* Physical drive nmuber (0..) */
+    BYTE cmd,  /* Control code */
+    void *buff /* Buffer to send/receive control data */
 )
 {
-	DRESULT res;
-	if(pdrv==SD_CARD)
-	{
-	    switch(cmd)
-	    {
-		    case CTRL_SYNC:
-				res = RES_OK;
-		        break;
-		    case GET_SECTOR_SIZE:
-				*(DWORD*)buff = 512;
-		        res = RES_OK;
-		        break;
-		    case GET_BLOCK_SIZE:
-				*(WORD*)buff = SDCardInfo.CardBlockSize;
-		        res = RES_OK;
-		        break;
-		    case GET_SECTOR_COUNT:
-		        *(DWORD*)buff = SDCardInfo.CardCapacity/512;
-		        res = RES_OK;
-		        break;
-		    default:
-		        res = RES_PARERR;
-		        break;
-	    }
-	}else res=RES_ERROR;
+    DRESULT res;
+    if (pdrv == SD_CARD)
+    {
+        switch (cmd)
+        {
+        case CTRL_SYNC:
+            res = RES_OK;
+            break;
+        case GET_SECTOR_SIZE:
+            *(DWORD *)buff = 512;
+            res = RES_OK;
+            break;
+        case GET_BLOCK_SIZE:
+            *(WORD *)buff = SDCardInfo.CardBlockSize;
+            res = RES_OK;
+            break;
+        case GET_SECTOR_COUNT:
+            *(DWORD *)buff = SDCardInfo.CardCapacity / 512;
+            res = RES_OK;
+            break;
+        default:
+            res = RES_PARERR;
+            break;
+        }
+    }
+    else
+        res = RES_ERROR;
     return res;
 }
 #endif
 
-DWORD get_fattime (void)
+DWORD get_fattime(void)
 {
-	return 0;
+    return 0;
 }
--- test/sdcard_copy/main.c
+++ test/sdcard_copy/main.c
@@ -1,6 +1,3 @@
-
-#include "nuclei_sdk_hal.h"
-
 #include <ctype.h>
 #include <string.h>
 #include <stdlib.h>
@@ -8,55 +5,65 @@
 #include "ff.h"
 #include "diskio.h"
 
-extern uint8_t CardType;		
+extern uint8_t CardType;
 extern uint32_t CSD_Tab[4];
 extern uint32_t CID_Tab[4];
-extern uint32_t RCA;					
+extern uint32_t RCA;
 extern SD_CardInfo SDCardInfo;
-#define FATFS_WR_SIZE 1024*8
+#define FATFS_WR_SIZE 1024 * 8
 
 void show_sdcard_info(void)
 {
-	switch(SDCardInfo.CardType)
-	{
-		case SDIO_STD_CAPACITY_SD_CARD_V1_1:printf("Card Type:SDSC V1.1\r\n");break;
-		case SDIO_STD_CAPACITY_SD_CARD_V2_0:printf("Card Type:SDSC V2.0\r\n");break;
-		case SDIO_HIGH_CAPACITY_SD_CARD:printf("Card Type:SDHC V2.0\r\n");break;
-		case SDIO_MULTIMEDIA_CARD:printf("Card Type:MMC Card\r\n");break;
-	}
-  	printf("Card ManufacturerID:%d\r\n",SDCardInfo.SD_cid.ManufacturerID);
- 	printf("Card RCA:%d\r\n",SDCardInfo.RCA);
-	printf("Card Capacity:%d MB\r\n",(uint32_t)(SDCardInfo.CardCapacity>>20));
- 	printf("Card BlockSize:%d\r\n\r\n",SDCardInfo.CardBlockSize);
+    switch (SDCardInfo.CardType)
+    {
+    case SDIO_STD_CAPACITY_SD_CARD_V1_1:
+        printf("Card Type:SDSC V1.1\r\n");
+        break;
+    case SDIO_STD_CAPACITY_SD_CARD_V2_0:
+        printf("Card Type:SDSC V2.0\r\n");
+        break;
+    case SDIO_HIGH_CAPACITY_SD_CARD:
+        printf("Card Type:SDHC V2.0\r\n");
+        break;
+    case SDIO_MULTIMEDIA_CARD:
+        printf("Card Type:MMC Card\r\n");
+        break;
+    }
+    printf("Card ManufacturerID:%d\r\n", SDCardInfo.SD_cid.ManufacturerID);
+    printf("Card RCA:%d\r\n", SDCardInfo.RCA);
+    printf("Card Capacity:%d MB\r\n", (uint32_t)(SDCardInfo.CardCapacity >> 20));
+    printf("Card BlockSize:%d\r\n\r\n", SDCardInfo.CardBlockSize);
 }
 
-__attribute__ ((aligned (4))) uint8_t buffer_all[FATFS_WR_SIZE];
-void copyfile(uint8_t * srcfilename, uint8_t * destfilename)
+__attribute__((aligned(4))) uint8_t buffer_all[FATFS_WR_SIZE];
+void copyfile(uint8_t *srcfilename, uint8_t *destfilename)
 {
 
-    UINT br,bw;
+    UINT br, bw;
     uint32_t f_res;
     FIL file_src;
     FIL file_dest;
 
-    f_res = f_open(&file_src, srcfilename, FA_OPEN_EXISTING | FA_READ  | FA_OPEN_ALWAYS);
+    f_res = f_open(&file_src, srcfilename, FA_OPEN_EXISTING | FA_READ | FA_OPEN_ALWAYS);
     if (f_res == FR_OK)
     {
-        f_res = f_open(&file_dest, destfilename, FA_CREATE_ALWAYS | FA_WRITE  | FA_READ | FA_OPEN_ALWAYS);
-            if(f_res!= FR_OK)
-            {
-                return ;
-            }
+        f_res = f_open(&file_dest, destfilename, FA_CREATE_ALWAYS | FA_WRITE | FA_READ | FA_OPEN_ALWAYS);
+        if (f_res != FR_OK)
+        {
+            return;
+        }
     }
     printf("start copy file.... \r\n");
     while (f_res == 0)
     {
-            f_res = f_read(&file_src, buffer_all, FATFS_WR_SIZE, &br);
-            printf("f_res = %d,br= %d   ", f_res, br);
-            if (f_res || br == 0) break; /* ÎÄ¼þ½áÊø´íÎó */
-            f_res = f_write(&file_dest, buffer_all, br, &bw);
-            printf("f_res = %d,br= %d,bw=%d \r\n", f_res, br, bw);
-            if (f_res || bw < br) break; /* ´ÅÅÌÂú´íÎó */
+        f_res = f_read(&file_src, buffer_all, FATFS_WR_SIZE, &br);
+        printf("f_res = %d,br= %d   ", f_res, br);
+        if (f_res || br == 0)
+            break; /* ÎÄ¼þ½áÊø´íÎó */
+        f_res = f_write(&file_dest, buffer_all, br, &bw);
+        printf("f_res = %d,br= %d,bw=%d \r\n", f_res, br, bw);
+        if (f_res || bw < br)
+            break; /* ´ÅÅÌÂú´íÎó */
     }
     f_close(&file_src);
     f_close(&file_dest);
@@ -65,22 +72,27 @@ void copyfile(uint8_t * srcfilename, uint8_t * destfilename)
 
 int main(void)
 {
-	int i,j;
-	FIL file;
-    FIL file1;                                                                
-	FATFS fatfs;                                                             
-	static FRESULT res;                                                      
-	unsigned int counter;
+    int i, j;
+    FIL file;
+    FIL file1;
+    FATFS fatfs;
+    static FRESULT res;
+    unsigned int counter;
 
-	res = f_mount(&fatfs,"0:",1);                                            
-	if(res != RES_OK){
-		printf("f_mount error:%d!\r\n",res);
-		while(1){}
-	}else{
-		printf("f_mount successful!\r\n");
-	}
- 
-    f_setlabel((const TCHAR *)"0:Nuclei");	
+    res = f_mount(&fatfs, "0:", 1);
+    if (res != RES_OK)
+    {
+        printf("f_mount error:%d!\r\n", res);
+        while (1)
+        {
+        }
+    }
+    else
+    {
+        printf("f_mount successful!\r\n");
+    }
 
-    copyfile("0:/fatfs.txt","0:/cp.txt");
+    f_setlabel((const TCHAR *)"0:Nuclei");
+
+    copyfile("0:/fatfs.txt", "0:/cp.txt");
 }
